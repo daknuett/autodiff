a744a34c2293e8ad9238837e31bed7c2c3e9d936
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2023-09-06 14:51:10 +0200

    everything but parallel layer working

--- autodiff.py
+++ autodiff.py
@@ -9,8 +9,11 @@ import numpy as np
 import typing
 import abc
 from collections import deque
+import itertools
 
 class Layer(metaclass=abc.ABCMeta):
+    def __init__(self):
+        self._dispatcher = None
     @abc.abstractmethod
     def random_weights(self):
         pass    
@@ -23,21 +26,34 @@ class Layer(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def dvin_proj(self, W, vin, r):
         pass
-
+    
+    @abc.abstractproperty
+    def nout(self):
+        pass
+    
+    
+    def get_weightlist_dispatcher(self):
+        return list(self.generate_and_set_weightlist_dispatcher(itertools.count()))
+    
+    @abc.abstractmethod
+    def generate_and_set_weightlist_dispatcher(self, cindex):
+        pass
+    
 class MatrixLayer(Layer):
     def __init__(self, nin: int, nout: int):
+        super().__init__()
         self._nin = nin
         self._nout = nout
         #self._weights = self.random_weights()
         
     def random_weights(self):
-        return np.random.uniform(-1, 1, (self._nout, self._nin))
+        yield np.random.uniform(-1, 1, (self._nout, self._nin))
     
     def call(self, W, vin):
-        return W @ vin
+        return W[self._dispatcher] @ vin
     
     def dweights_proj(self, W, vin, r):
-        dW = np.zeros_like(W)
+        dW = np.zeros_like(W[self._dispatcher])
         for i in range(vin.shape[0]):
             dW[:,i] = r
         for j in range(r.shape[0]):
@@ -45,17 +61,26 @@ class MatrixLayer(Layer):
         return dW
     
     def dvin_proj(self, W, vin, r):
-        return W.T @ r
+        return W[self._dispatcher].T @ r
+    
+    @property
+    def nout(self):
+        return self._nout
+    
+    def generate_and_set_weightlist_dispatcher(self, cindex):
+        self._dispatcher = next(cindex)
+        yield self._dispatcher
     
 class ReluLayer(Layer):
     def __init__(self, nvals: int):
+        super().__init__()
         self._nvals = nvals
         
     def random_weights(self):
-        return np.random.uniform(-1, 1, self._nvals)
+        yield np.random.uniform(-1, 1, self._nvals)
     
     def call(self, W, vin):
-        return W * (vin > 0) * vin
+        return W[self._dispatcher] * (vin > 0) * vin
     
     def dweights_proj(self, W, vin, r):
         dW = np.copy(r)
@@ -63,38 +88,189 @@ class ReluLayer(Layer):
         return dW
     
     def dvin_proj(self, W, vin, r):
-        dv = np.copy(W)
+        dv = np.copy(W[self._dispatcher])
         dv *= r * (vin > 0)
         return dv
     
+    @property
+    def nout(self):
+        return self._nvals
+    def generate_and_set_weightlist_dispatcher(self, cindex):
+        self._dispatcher = next(cindex)
+        yield self._dispatcher
+        
 class BiasLayer(Layer):
     def __init__(self, nvals: int):
+        super().__init__()
         self._nvals = nvals
         
     def random_weights(self):
-        return np.random.uniform(-1, 1, self._nvals)
+        yield np.random.uniform(-1, 1, self._nvals)
     
     def call(self, W, vin):
-        return W + vin
+        return W[self._dispatcher] + vin
     
     def dweights_proj(self, W, vin, r):
         return np.copy(r)
     
     def dvin_proj(self, W, vin, r):
         return np.copy(r)
-
-class Model:
-    def __init__(self, layers: typing.List[Layer]):
+    
+    @property
+    def nout(self):
+        return self._nvals
+    def generate_and_set_weightlist_dispatcher(self, cindex):
+        self._dispatcher = next(cindex)
+        yield self._dispatcher
+        
+class SequenceLayer(Layer):
+    def __init__(self, layers):
+        super().__init__()
         self._layers = layers
         
     def random_weights(self):
-        return [l.random_weights() for l in self._layers]
+        for l in self._layers:
+            yield from l.random_weights()
     
     def call(self, W, vin):
         v = vin
-        for w, l in zip(W, self._layers):
-            v = l.call(w, v)
+        for l in self._layers:
+            v = l.call(W, v)
         return v
+    
+    def dweights_proj(self, W, vin, r):
+        # Forward propagation of values
+        values = [vin]
+        for l in self._layers:
+            values.append(l.call(W, values[-1]))
+    
+        # Backward accumulation of gradients
+        gradients = deque()
+        right = r
+        for l, h in zip(reversed(self._children), reversed(values[:-1])):
+            gradients.appendleft(l.dweights_proj(W, h, right))
+            right = l.dvin_proj(W, h, right)
+    
+        return list(gradients)
+    
+    def dvin_proj(self, W, vin, r):
+        # Forward propagation of values
+        values = [vin]
+        for l in self._layers:
+            values.append(l.call(W, values[-1]))
+    
+        # Backward accumulation of gradients
+        right = r
+        for l, h in zip(reversed(self._children), reversed(values[:-1])):
+            right = l.dvin_proj(W, h, right)
+            
+        return right
+    
+    @property
+    def nout(self):
+        return self._layers[-1].nout
+    def generate_and_set_weightlist_dispatcher(self, cindex):
+        for layer in self._layers:
+            yield from layer.generate_and_set_weightlist_dispatcher(cindex)
+        
+class IdentityLayer(Layer):
+    def __init__(self, npass):
+        super().__init__()
+        self._npass = npass
+    
+    @property
+    def nout(self):
+        return self._npass
+    
+    def random_weights(self):
+        yield np.array([0])
+    
+    def dweights_proj(self, W, vin, r):
+        return np.array([0])
+    
+    def dvin_proj(self, W, vin, r):
+        return np.copy(r)
+    
+    def call(self, W, vin):
+        return np.copy(vin)
+    def generate_and_set_weightlist_dispatcher(self, cindex):
+        self._dispatcher = next(cindex)
+        yield self._dispatcher
+        
+class ParallelLayer(Layer):
+    def __init__(self, nin, sequence_a: Layer, sequence_b: Layer):
+        super().__init__()
+        self._nin = nin
+        self._sequence_a = sequence_a
+        self._sequence_b = sequence_b
+    
+    @property
+    def nout(self):
+        return self._nin
+    
+    def call(self, W, vin):
+        v1 = np.copy(vin)
+        v2 = np.copy(vin)
+        
+        r1 = self._sequence_b.call(W, v1)
+        r2 = self._sequence_b.call(W, v2)
+        
+        r = np.hstack([r1, r2])
+        return  W[self._dispatcher] @ r
+        
+    def random_weights(self):
+        yield from self._sequence_a.random_weights()
+        yield from self._sequence_b.random_weights()
+        yield np.random.uniform(-1, 1, (self.nout, self._sequence_a.nout + self._sequence_b.nout))
+        
+    def dweights_proj(self, W, vin, r):
+        #v1 = np.copy(vin)
+        #v2 = np.copy(vin)
+        #
+        #r1 = self._sequence_b.call(W, v1)
+        #r2 = self._sequence_b.call(W, v2)
+        #
+        #r = np.hstack([r1, r2])
+        
+        dW = np.zeros_like(W[self._dispatcher])
+        for i in range(vin.shape[0]):
+            dW[:,i] = r
+        for j in range(r.shape[0]):
+            dW[j,:] *= vin
+        return dW
+    
+    def dvin_proj(self, W, vin, r):
+        v1 = np.copy(vin)
+        v2 = np.copy(vin)
+        
+        r1 = self._sequence_b.call(W, v1)
+        r2 = self._sequence_b.call(W, v2)
+        
+        r = np.hstack([r1, r2])
+        return W[self._dispatcher].T @ r
+        
+    def generate_and_set_weightlist_dispatcher(self, cindex):
+        yield from self._sequence_a.generate_and_set_weightlist_dispatcher(cindex)
+        yield from self._sequence_b.generate_and_set_weightlist_dispatcher(cindex)
+        self._dispatcher = next(cindex)
+        yield self._dispatcher
+    
+       
+
+class Model(SequenceLayer):
+    def __init__(self, layers: typing.List[Layer]):
+        super().__init__(layers)
+        self.get_weightlist_dispatcher()
+        
+    def get_random_weights(self):
+        return list(self.random_weights())
+        
+    #def call(self, W, vin):
+    #    v = vin
+    #    for w, l in zip(W, self._layers):
+    #        v = l.call(w, v)
+    #    return v
+    #
     
     def cost(self, W, vin, b):
         return np.linalg.norm(self.call(W, vin) - b)**2
@@ -102,15 +278,15 @@ class Model:
     def dcost(self, W, vin, b):
         # Forward propagation of values
         values = [vin]
-        for w, l in zip(W, self._layers):
-            values.append(l.call(w, values[-1]))
+        for l in self._layers:
+            values.append(l.call(W, values[-1]))
         
         # Backward accumulation of gradients
         right = (values[-1] - b)
         gradients = deque()
-        for (w, l), h in zip(zip(reversed(W), reversed(self._layers)), reversed(values[:-1])):
-            gradients.appendleft(l.dweights_proj(w, h, right))
-            right = l.dvin_proj(w, h, right)
+        for l, h in zip(reversed(self._layers), reversed(values[:-1])):
+            gradients.appendleft(l.dweights_proj(W, h, right))
+            right = l.dvin_proj(W, h, right)
     
         return list(gradients)
 
